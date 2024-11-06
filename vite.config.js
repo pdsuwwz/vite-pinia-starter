cf105f8dbc444d0cf030cdd3e06dec287e75fb23
Wisdom <pdsu.wwz@foxmail.com>
2024-11-06 14:23:07 +0800

    🔥 chore: support vitest (#175)
    * 🔥 chore: support vitest
    
    * 🔥 fix: sass warning

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 import path from 'path'
@@ -60,8 +60,20 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        api: 'modern',
         additionalData: `@use '@/styles/element-variables.scss' as *;`
       }
     }
+  },
+  test: {
+    globals: true,
+    dir: '__tests__',
+    environment: 'jsdom',
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
   }
 })
